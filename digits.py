5961a052c2c7a962ea3797dfe2e36107a6d0ef8a
roth141 <roth141@mail.chapman.edu>
2016-03-17 20:34:22 -0700

    digits

---
+++ digits.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 17 20:19:08 2016
+
+@author: LydiaRoth
+"""
+
+def digits(n):
+    string = str(n)
+    return  len(string)  
+print digits (-4530)    
